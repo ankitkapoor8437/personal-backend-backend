1d389b14a38de234de9d2360e0d6699eadeaf8a3
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-07-18 00:39:47 +0530

    Cross origin reference.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { errorHandler } = require('./middleware/errorHandler');
 const { connectDb } = require('./config/dbConnetions');
+const cors = require('cors');
+
 
 const dotenv = require("dotenv").config();
 connectDb();
@@ -8,10 +10,14 @@ connectDb();
 const app = express();
 const port = 9000;
 
+const cors = require('cors');
+
 // app.use("/", (req, res) => {
 //   res.json({ message: "Hello from express app!" });
 // });
 
+
+app.use(cors());
 app.use(express.json());
 app.use("/api/projects", require("./routes/projectRoute"));
 app.use(errorHandler);
